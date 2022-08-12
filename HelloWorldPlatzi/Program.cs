107a943dedd436c13f9e32d6d7026e5d39453211
Jonathan Rodriguez Ramirez <jonathan.voices@gmail.com>
2022-08-12 17:46:06 -0500

    Manejo de int

--- HelloWorldPlatzi/Program.cs
+++ HelloWorldPlatzi/Program.cs
@@ -6,9 +6,21 @@ namespace HelloWorldPlatzi
     {
         static void Main(string[] args)
         {
+            int number1;
+            int number2;
+            int result;
+
             Console.WriteLine("What's you name? Please write your full name");
             string fullUserName = Console.ReadLine();
-            Console.WriteLine("Hello!! " + fullUserName + ", Welcome to platzi");
+                        
+            Console.WriteLine("Input the first number");
+            number1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Input the second number");
+            number2 = Convert.ToInt32(Console.ReadLine());
+            
+            result = number1 + number2;
+            Console.WriteLine("Hello!! " + fullUserName + ", Welcome to platzi, the result is: " + result);
 
         }
     }
