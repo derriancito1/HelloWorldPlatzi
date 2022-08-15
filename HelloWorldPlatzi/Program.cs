e9b1bbcd105b01bbea4f2e8bb35678c7fe0380ea
Jonathan Rodriguez Ramirez <jonathan.voices@gmail.com>
2022-08-15 10:17:23 -0500

    Operadores Logicos

--- HelloWorldPlatzi/Program.cs
+++ HelloWorldPlatzi/Program.cs
@@ -6,39 +6,20 @@ namespace HelloWorldPlatzi
     {
         static void Main(string[] args)
         {
-            //int number1;
-            //int number2;
-            //int result;
-            //double height = 1.7;
-            //int age = 20;
-            //string name = "Jonathan Rodriguez";
-            //string information = "The Information you requested is: \nUser Name: " + name + "\nUser Age: " + age + "\nUser Height: " + height;
+            bool value1 = true;
+            bool value2 = true;
+            bool value3 = false;
 
+            // &&, ||, !
 
+            bool result = value1 && value2 && value3;
+            Console.WriteLine("The result of the logical AND is; " + result);
 
+            bool result2 = value1 || value2 || value3;
+            Console.WriteLine("The result of the logical OR is; " + result2);
 
-            //Console.WriteLine("What's you name? Please write your full name?");
-            //name = Console.ReadLine();
-
-            //Console.WriteLine("Input the first number");
-            //number1 = Convert.ToInt32(Console.ReadLine());
-
-            //Console.WriteLine("Input the second number");
-            //number2 = Convert.ToInt32(Console.ReadLine());
-
-            //result = number1 + number2;
-            //Console.WriteLine("Hello!! " + name + ", Welcome to platzi, the result is: " + result);
-
-            //Console.WriteLine(information);
-
-
-            //rectangle area canculation.
-            float sideA = 10;
-            float sideB = 20;
-
-            float area = sideA * sideB;
-
-            Console.WriteLine("The reactangle area is: " + area);
+            bool result3 = !value1;
+            Console.WriteLine("The result of the logical ! is; " + result3);
 
         }
     }
