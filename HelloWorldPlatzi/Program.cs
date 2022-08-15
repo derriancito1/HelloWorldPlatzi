4d9313f9ebb54911c42a045085a82fc2a63773a9
Jonathan Rodriguez Ramirez <jonathan.voices@gmail.com>
2022-08-15 13:27:58 -0500

    Metodos propios

--- HelloWorldPlatzi/Program.cs
+++ HelloWorldPlatzi/Program.cs
@@ -5,19 +5,32 @@ namespace HelloWorldPlatzi
 {
     internal class Program
     {
+        class MisMetodos
+        {
+            public void IntegerAddition(int a, int b)
+            {
+                int Addition = a + b;
+                Console.WriteLine("El resultado es " + Addition);
+            }
+
+            public int IntegerMultiplicacion(int a, int b)
+            {
+                int Multiplication = a * b;
+                return Multiplication;
+            }
+
+            public float IntegerDivision(int a, int b)
+            {
+                float Division = a / b;
+                return Division;
+            }
+        }
         static void Main(string[] args)
         {
-            float[] pricesArray;
-
-            pricesArray = new float[10];
-            pricesArray[0] = 1.5f;
-            pricesArray[1] = 2.5f;
-            pricesArray[2] = 3f;
-            pricesArray[3] = 5f;
-
-            Random random = new Random();
-            Console.WriteLine("Hello world, of random numbers!");
-            Console.WriteLine($"{random.Next(1,10)}");
+            MisMetodos myprogram = new MisMetodos();
+            myprogram.IntegerAddition(3, 9);
+            Console.WriteLine("El resultado de la multiplicacion es: " + myprogram.IntegerMultiplicacion(3, 9));
+            Console.WriteLine("El resultado de la division es: " + myprogram.IntegerDivision(18, 9));
 
         }
     }
