f3af1313bd5de24c814770623ad002ccd36a8b40
Jonathan Rodriguez Ramirez <jonathan.voices@gmail.com>
2022-08-16 14:34:57 -0500

    Ciclo while

--- HelloWorldPlatzi/Program.cs
+++ HelloWorldPlatzi/Program.cs
@@ -6,11 +6,52 @@ namespace HelloWorldPlatzi
     internal class Program
     {
         static void Main(string[] args)
-        {
-            for (int i = 1; i <= 50; i++)
+        {   
+            bool continueSoftwareExecution = false;
+            while (continueSoftwareExecution == true)
             {
-                Console.WriteLine("Hello world! #{0}",i);
+                Console.WriteLine("Do you wish to keep the software running? write 1 if yes, 0 if no");
+                int keepGoing = Convert.ToInt16(Console.ReadLine());
+                if (keepGoing==1)
+                {
+                    Console.WriteLine("Hello workd! The software will keep running");
+                    continueSoftwareExecution = true;
+                }
+                else if (keepGoing==0)
+                {
+                    Console.WriteLine("This is the last time the software is running");
+                    continueSoftwareExecution = false;
+                }
+                else
+                {
+                    Console.WriteLine("Invalid input, Try Again");
+                    continueSoftwareExecution = true;
+                }
             }
+
+
+            do
+            {
+                Console.WriteLine("Do you wish to keep the software running? write 1 if yes, 0 if no");
+                int keepGoing = Convert.ToInt16(Console.ReadLine());
+                if (keepGoing == 1)
+                {
+                    Console.WriteLine("Hello workd! The software will keep running");
+                    continueSoftwareExecution = true;
+                }
+                else if (keepGoing == 0)
+                {
+                    Console.WriteLine("This is the last time the software is running");
+                    continueSoftwareExecution = false;
+                }
+                else
+                {
+                    Console.WriteLine("Invalid input, Try Again");
+                    continueSoftwareExecution = true;
+                }
+            } while (continueSoftwareExecution == true);
+
+
         }
     }
 }
