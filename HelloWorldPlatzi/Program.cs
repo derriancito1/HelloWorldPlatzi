67c22de73d63dae156f4a9efafb98305f8623009
Jonathan Rodriguez Ramirez <jonathan.voices@gmail.com>
2022-08-16 14:17:13 -0500

    Ciclo for

--- HelloWorldPlatzi/Program.cs
+++ HelloWorldPlatzi/Program.cs
@@ -7,28 +7,10 @@ namespace HelloWorldPlatzi
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Enter the selected soda: ");
-            string caseSwitch = Console.ReadLine();
-
-            switch (caseSwitch)
+            for (int i = 1; i <= 50; i++)
             {
-                case "cola":
-                    Console.WriteLine("Cola soda - $2 USD");
-                    break;
-                case "lime":
-                    Console.WriteLine("Lime soda - $1 USD");
-                    break;
-                case "orange":
-                    Console.WriteLine("Orange soda - $1.5 USD");
-                    break;
-                case "apple":
-                    Console.WriteLine("Apple soda - $1 USD");
-                    break;
-                default:
-                    Console.WriteLine("ERROR: You did not select a soda or you entered an incorrect value.");
-                    break;
+                Console.WriteLine("Hello world! #{0}",i);
             }
-
         }
     }
 }
