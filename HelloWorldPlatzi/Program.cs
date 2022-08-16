06ed48759abe5fe9b7663bc2976ab2048476ce1b
Jonathan Rodriguez Ramirez <jonathan.voices@gmail.com>
2022-08-16 15:17:16 -0500

    Proyecto lista de usuarios

--- HelloWorldPlatzi/Program.cs
+++ HelloWorldPlatzi/Program.cs
@@ -1,57 +1,64 @@
 ﻿using System;
-using System.Collections.Generic;
+
+
+/*
+ * 1. Validar si es usuario existente o si se debe registrar y generar un sistema de registro o login
+ * 2. El programa debe ser capaz de darle la bienvenida a un usuario existente si en efecto existe
+ * 3. El programa debe repetirse hasta que se registren los 10 comensales 
+ * */
+
 
 namespace HelloWorldPlatzi
 {
     internal class Program
     {
         static void Main(string[] args)
-        {   
-            bool continueSoftwareExecution = false;
-            while (continueSoftwareExecution == true)
+        {
+            string[] userNames = new string[10]{"Pedro", "Robert", "Joan", "Selena", "Mike", "Dani", "Platz", "Lexie", "Danae", ""};
+            int arrayCurrentIndex = 9;
+            bool userType;
+
+            Console.WriteLine("Welcome to the best restaurant in the world!");
+            while (arrayCurrentIndex<10)
             {
-                Console.WriteLine("Do you wish to keep the software running? write 1 if yes, 0 if no");
-                int keepGoing = Convert.ToInt16(Console.ReadLine());
-                if (keepGoing==1)
+                /*if (arrayCurrentIndex==10)
                 {
-                    Console.WriteLine("Hello workd! The software will keep running");
-                    continueSoftwareExecution = true;
+                    Console.WriteLine("The restaurant is full, try again next year");
+                    Environment.Exit(0);
+                }*/
+                Console.WriteLine("\n \nAre you a registerd user? write true, or write false to register");
+                userType = Convert.ToBoolean(Console.ReadLine());
+                if (userType)
+                {
+                    Console.WriteLine("Hello, you are a registerd user, please enter your exact user name");
+                    string userToSearch = Console.ReadLine();
+                    Console.WriteLine("The user you searched is {0}", userToSearch);
+                    int index = Array.IndexOf(userNames, userToSearch);
+                    if (index == -1)
+                    {
+                        Console.WriteLine("User not found, try again or register");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Welcome {0}, it's a pleasure to give you food", userNames[index]);
+                    }
                 }
-                else if (keepGoing==0)
+                else if (!userType)
                 {
-                    Console.WriteLine("This is the last time the software is running");
-                    continueSoftwareExecution = false;
-                }
-                else
-                {
-                    Console.WriteLine("Invalid input, Try Again");
-                    continueSoftwareExecution = true;
+                    Console.WriteLine("Please write and remember your username");
+                    userNames[arrayCurrentIndex] = Console.ReadLine();
+                    Console.WriteLine("Your user has been saved succesfully \n" + "your username is> {0}", userNames[arrayCurrentIndex]);
+                    arrayCurrentIndex++;
                 }
             }
-
-
-            do
+            Console.WriteLine("The restaurant is full, try again next year \n This are the guest to the dinner:");
+            int auxIndex = 0;
+            foreach (string item in userNames)
             {
-                Console.WriteLine("Do you wish to keep the software running? write 1 if yes, 0 if no");
-                int keepGoing = Convert.ToInt16(Console.ReadLine());
-                if (keepGoing == 1)
-                {
-                    Console.WriteLine("Hello workd! The software will keep running");
-                    continueSoftwareExecution = true;
-                }
-                else if (keepGoing == 0)
-                {
-                    Console.WriteLine("This is the last time the software is running");
-                    continueSoftwareExecution = false;
-                }
-                else
-                {
-                    Console.WriteLine("Invalid input, Try Again");
-                    continueSoftwareExecution = true;
-                }
-            } while (continueSoftwareExecution == true);
-
-
+                Console.WriteLine("User Number {0} and user name: {1}", auxIndex+1, userNames[auxIndex]);
+                auxIndex++;
+            }
+            Environment.Exit(0);
         }
     }
 }
